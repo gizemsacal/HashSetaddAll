1752a95b4e627a4b9f3215a16670827b36894e4d
gizem <gizemsacal@gmail.com>
2021-05-07 16:48:07 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.HashSet;
+
+class Main {
+    public static void main(String[] args) {
+        HashSet<Integer> evenNumbers = new HashSet<>();
+        evenNumbers.add(2);
+        evenNumbers.add(4);
+        System.out.println("HashSet1: " + evenNumbers);
+
+        HashSet<Integer> numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(3);
+        System.out.println("HashSet2: " + numbers);
+
+        // Union of two set
+        numbers.addAll(evenNumbers);
+        System.out.println("Birleşim: " + numbers);
+    }
+}
